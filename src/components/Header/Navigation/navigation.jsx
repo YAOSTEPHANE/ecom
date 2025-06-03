479c800e613efea67d4810e1fccb08d21cd70354
YAOSTEPHANE <kouassistephane489@gmail.com>
2025-06-03 14:03:26 +0000

    commit

--- src/components/Header/Navigation/navigation.jsx
+++ src/components/Header/Navigation/navigation.jsx
@@ -8,107 +8,199 @@ import CategoryPanel from "./CategoryPanel";
 import "../Navigation/style.css"; // Assuming you have a CSS file for styles
 
 const Navigation = () => {
-
   const [isOpenCatPanel, setIsOpenCatPanel] = useState(false);
 
   const openCategoryPanel = () => {
     setIsOpenCatPanel(true);
   };
 
- 
-
-
-
   return (
     <>
-    <nav className="py-2">
-      <div className="container flex items-center justify-end gap-8">
-        <div className="col_1 w-[20%]">
-          <Button className="!text-[black] gap-2 w-full" onClick={openCategoryPanel}>
-            <RiMenu2Fill className="text-[18px]" /> Catégories{" "}
-            <TfiAngleDown className="text-[13px] ml-auto font-bold" />
-          </Button>
+      <nav className="py-2">
+        <div className="container flex items-center justify-end gap-8">
+          <div className="col_1 w-[20%]">
+            <Button
+              className="!text-[black] gap-2 w-full"
+              onClick={openCategoryPanel}
+            >
+              <RiMenu2Fill className="text-[18px]" /> Catégories{" "}
+              <TfiAngleDown className="text-[13px] ml-auto font-bold" />
+            </Button>
+          </div>
+          <div className="col_2 w-[60%]">
+            <ul className="flex items-center gap-1 nav">
+              <li className="list-none">
+                <Link to="/" className="link transition text-[14px] font-[500]">
+                  <Button className="link transition font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#FF5252] ">
+                    Accueil
+                  </Button>
+                </Link>
+              </li>
+              <li className="list-none relative">
+                <Link to="/" className="link transition text-[13px] font-[500]">
+                  <Button className="link transition font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#FF5252]">
+                    Mode
+                  </Button>
+                </Link>
+
+                <div className="submenu absolute top-[120%] left-[0%] min-w-[150px] bg-white shadow-md opacity-0 transition-all">
+                  <ul>
+                    <li className="list-none w-full relative">
+                      <Link to="/" className="w-full">
+                        <Button className="!text-[rgba(0,0,0,0.8)] w-full !text-left !justify-start rounded-none">
+                          Hommes
+                        </Button>
+
+                        <div className="submenu absolute top-[0%] left-[100%] min-w-[150px] bg-white shadow-md opacity-0 transition-all">
+                          <ul>
+                            <li className="list-none w-full">
+                              <Link to="/" className="w-full">
+                                <Button className="!text-[rgba(0,0,0,0.8)] w-full !text-left !justify-start !rounded-none">
+                                  T-shirts
+                                </Button>
+                              </Link>
+                            </li>
+                            <li className="list-none w-full">
+                              <Link to="/" className="w-full">
+                                <Button className="!text-[rgba(0,0,0,0.8)] w-full !text-left !justify-start !rounded-none">
+                                  Pantalons
+                                </Button>
+                              </Link>
+                            </li>
+                            <li className="list-none w-full">
+                              <Link to="/" className="w-full">
+                                <Button className="!text-[rgba(0,0,0,0.8)] w-full !text-left !justify-start rounded-none">
+                                  Chaussures
+                                </Button>
+                              </Link>
+                            </li>
+                            <li className="list-none w-full">
+                              <Link to="/" className="w-full">
+                                <Button className="!text-[rgba(0,0,0,0.8)] w-full !text-left !justify-start rounded-none">
+                                  Vestes
+                                </Button>
+                              </Link>
+                            </li>
+                            <li className="list-none w-full">
+                              <Link to="/" className="w-full">
+                                <Button className="!text-[rgba(0,0,0,0.8)] w-full !text-left !justify-start rounded-none">
+                                  Chemises
+                                </Button>
+                              </Link>
+                            </li>
+                            <li className="list-none w-full">
+                              <Link to="/" className="w-full">
+                                <Button className="!text-[rgba(0,0,0,0.8)] w-full !text-left !justify-start rounded-none">
+                                  Accessoires
+                                </Button>
+                              </Link>
+                            </li>
+                          </ul>
+                        </div>
+                      </Link>
+                    </li>
+                    <li className="list-none w-full">
+                      <Link to="/" className="w-full">
+                        <Button className="!text-[rgba(0,0,0,0.8)] w-full !text-left !justify-start !rounded-none">
+                          Femmes
+                        </Button>
+                      </Link>
+                    </li>
+                    <li className="list-none w-full">
+                      <Link to="/" className="w-full">
+                        <Button className="!text-[rgba(0,0,0,0.8)] w-full !text-left !justify-start rounded-none">
+                          Enfants
+                        </Button>
+                      </Link>
+                    </li>
+                    <li className="list-none w-full">
+                      <Link to="/" className="w-full">
+                        <Button className="!text-[rgba(0,0,0,0.8)] w-full !text-left !justify-start rounded-none">
+                          Bebes
+                        </Button>
+                      </Link>
+                    </li>
+                    <li className="list-none w-full">
+                      <Link to="/" className="w-full">
+                        <Button className="!text-[rgba(0,0,0,0.8)] w-full !text-left !justify-start rounded-none">
+                          Filles
+                        </Button>
+                      </Link>
+                    </li>
+                    <li className="list-none w-full">
+                      <Link to="/" className="w-full">
+                        <Button className="!text-[rgba(0,0,0,0.8)] w-full !text-left !justify-start rounded-none">
+                          Garçons
+                        </Button>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li className="list-none">
+                <Link to="/" className="link transition text-[13px] font-[500]">
+                  <Button className="link transition font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#FF5252] ">
+                    Electroniques
+                  </Button>
+                </Link>
+              </li>
+              <li className="list-none">
+                <Link to="/" className="link transition text-[13px] font-[500]">
+                  <Button className="link transition font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#FF5252] ">
+                    Hygiène
+                  </Button>
+                </Link>
+              </li>
+              <li className="list-none">
+                <Link to="/" className="link transition text-[13px] font-[500]">
+                  <Button className="link transition font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#FF5252] ">
+                    Maison
+                  </Button>
+                </Link>
+              </li>
+              <li className="list-none">
+                <Link to="/" className="link transition text-[13px] font-[500]">
+                  <Button className="link transition font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#FF5252] ">
+                    Electromenager{" "}
+                  </Button>
+                </Link>
+              </li>
+              <li className="list-none">
+                <Link to="/" className="link transition text-[13px] font-[500]">
+                  <Button className="link transition font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#FF5252] ">
+                    Informatique
+                  </Button>
+                </Link>
+              </li>
+              <li className="list-none">
+                <Link to="/" className="link transition text-[13px] font-[500]">
+                  <Button className="link transition font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#FF5252] ">
+                    Beauté
+                  </Button>
+                </Link>
+              </li>
+              <li className="list-none">
+                <Link to="/" className="link transition text-[13px] font-[500]">
+                  <Button className="link transition font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#ff5252] ">
+                    Bureau
+                  </Button>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="col_3 w-[20%]">
+            <p className="text-[14px] font-[500] flex items-center gap-3 mb-0 mt-0">
+              <GoRocket className="text-[18px]" />
+              Livraison rapide en 24H
+            </p>
+          </div>
         </div>
-        <div className="col_2 w-[60%]">
-          <ul className="flex items-center gap-1">
-            <li className="list-none">
-              <Link to="/" className="link transition text-[13px] font-[500]">
-                <Button className="link transition font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#FF5252] ">
-                  Accueil
-                </Button>
-              </Link>
-            </li>
-            <li className="list-none">
-              <Link to="/" className="link transition text-[13px] font-[500]">
-                <Button className="link transition font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#FF5252]">
-                  Mode
-                </Button>
-              </Link>
-            </li>
-            <li className="list-none">
-              <Link to="/" className="link transition text-[13px] font-[500]">
-                <Button className="link transition font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#FF5252] ">
-                  Electroniques
-                </Button>
-              </Link>
-            </li>
-            <li className="list-none">
-              <Link to="/" className="link transition text-[13px] font-[500]">
-                <Button className="link transition font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#FF5252] ">
-                  Hygiène
-                </Button>
-              </Link>
-            </li>
-            <li className="list-none">
-              <Link to="/" className="link transition text-[13px] font-[500]">
-                <Button className="link transition font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#FF5252] ">
-                  Maison
-                </Button>
-              </Link>
-            </li>
-            <li className="list-none">
-              <Link to="/" className="link transition text-[13px] font-[500]">
-                <Button className="link transition font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#FF5252] ">
-                  Electromenager{" "}
-                </Button>
-              </Link>
-            </li>
-            <li className="list-none">
-              <Link to="/" className="link transition text-[13px] font-[500]">
-                <Button className="link transition font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#FF5252] ">
-                  Informatique
-                </Button>
-              </Link>
-            </li>
-            <li className="list-none">
-              <Link to="/" className="link transition text-[13px] font-[500]">
-                <Button className="link transition font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#FF5252] ">
-                  Beauté
-                </Button>
-              </Link>
-            </li>
-            <li className="list-none">
-              <Link to="/" className="link transition text-[13px] font-[500]">
-                <Button className="link transition font-[500] !text-[rgba(0,0,0,0.8)] hover:!text-[#ff5252] ">
-                  Bureau
-                </Button>
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="col_3 w-[20%]">
-          <p className="text-[14px] font-[500] flex items-center gap-3 mb-0 mt-0">
-            <GoRocket className="text-[18px]" />
-            Livraison rapide en 24H
-          </p>
-        </div>
-      </div>
-    </nav>
+      </nav>
 
-
-
-
-    <CategoryPanel isOpenCatPanel={isOpenCatPanel} setIsOpenCatPanel={setIsOpenCatPanel}  />
+      <CategoryPanel
+        isOpenCatPanel={isOpenCatPanel}
+        setIsOpenCatPanel={setIsOpenCatPanel}
+      />
     </>
   );
 };
