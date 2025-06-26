326f1f5d3153f35e3595405d51f60f17ac067bf9
YAOSTEPHANE <kouassistephane489@gmail.com>
2025-06-26 15:34:20 +0000

    ecom

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,23 +1,32 @@
 import React from 'react'
-import FormGroup from '@mui/material/FormGroup';
+import "../Sidebar/style.css"
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
+import {Collapse} from 'react-collapse';
+import { FaAngleDown } from "react-icons/fa6";
+import Button from "@mui/material/Button"
 
 const Sidebar = () => {
+
+    
   return (
     <aside className='sidebar py-5'>
         <div className='box'>
-           <h3 className='mb-3 text-[18px] font-[600]'>Achetez par Categorie</h3>
-           <div className='scroll'>
-            <FormControlLabel control={<Checkbox />} label="Mode" />
-            <FormControlLabel control={<Checkbox />} label="Electroniques" />
-            <FormControlLabel control={<Checkbox />} label="Hygiène" />
-            <FormControlLabel control={<Checkbox />} label="Maison" />
-            <FormControlLabel control={<Checkbox />} label="Electromanagers" />
-            <FormControlLabel control={<Checkbox />} label="Informatique" />
-            <FormControlLabel control={<Checkbox />} label="Beauté" />
-            <FormControlLabel control={<Checkbox />} label="Bureau" />
+           <h3 className='mb-3 text-[18px] font-[600] flex items-center'>Achetez par Categorie
+            <Button><FaAngleDown /></Button>
+           </h3>
+           <Collapse isOpened={false}>
+           <div className='scroll px-3 relative -left-[-10px]'>
+            <FormControlLabel control={<Checkbox size='small' />} label="Mode" className='w-full' />
+            <FormControlLabel control={<Checkbox size='small' />} label="Electroniques" className='w-full' />
+            <FormControlLabel control={<Checkbox size='small' />} label="Hygiène" className='w-full' />
+            <FormControlLabel control={<Checkbox size='small' />} label="Maison" className='w-full' />
+            <FormControlLabel control={<Checkbox size='small' />} label="Electromanagers" className='w-full' />
+            <FormControlLabel control={<Checkbox  size='small'/>} label="Informatique" className='w-full' />
+            <FormControlLabel control={<Checkbox size='small' />} label="Beauté" className='w-full' />
+            <FormControlLabel control={<Checkbox size='small' />} label="Bureau" className='w-full' />
            </div>
+           </Collapse>
         </div>
     </aside>
   )
