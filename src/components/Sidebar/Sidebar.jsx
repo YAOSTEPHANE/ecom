cd3d76ad4e11c521ff79e45d7172c5586ba5ba80
YAOSTEPHANE <kouassistephane489@gmail.com>
2025-06-26 15:46:17 +0000

    ecom

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../Sidebar/style.css"
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
@@ -8,14 +8,15 @@ import Button from "@mui/material/Button"
 
 const Sidebar = () => {
 
-    
+    const [isOpenCategoryFilter, SetIsOpenCategoryFilter] = useState(true)
+
   return (
     <aside className='sidebar py-5'>
         <div className='box'>
-           <h3 className='mb-3 text-[18px] font-[600] flex items-center'>Achetez par Categorie
-            <Button><FaAngleDown /></Button>
+           <h3 className='w-full mb-3 text-[18px] font-[600] flex items-center pr-5'>Achetez par Categorie
+            <Button className='!w-[30px] !h-[30px] !min-w-[30px] !rounded-full !ml-auto' onClick={() => SetIsOpenCategoryFilter(!isOpenCategoryFilter)}><FaAngleDown /></Button>
            </h3>
-           <Collapse isOpened={false}>
+           <Collapse isOpened={isOpenCategoryFilter}>
            <div className='scroll px-3 relative -left-[-10px]'>
             <FormControlLabel control={<Checkbox size='small' />} label="Mode" className='w-full' />
             <FormControlLabel control={<Checkbox size='small' />} label="Electroniques" className='w-full' />
